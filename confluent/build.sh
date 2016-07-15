7b9c51987db2e96f38ab586f2f16dc37a345c00e
wlezzar <walid.lezzar@gmail.com>
2016-07-15 23:03:55 +0200

    Add a zookeeper-exhibitor image

--- confluent/build.sh
+++ confluent/build.sh
@@ -9,6 +9,7 @@ fi
 TAG=$1
 
 docker build -t wlezzar/zookeeper:$TAG zookeeper
+docker build -t wlezzar/zookeeper-exhibitor:$TAG zookeeper-exhibitor
 docker build -t wlezzar/kafka:$TAG kafka
 docker build -t wlezzar/schema-registry:$TAG schema-registry
 docker build -t wlezzar/kafka-rest:$TAG kafka-rest
