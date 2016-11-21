e067976c54b0fb3d32069a02276d805b23e29292
wlezzar <walid.lezzar@gmail.com>
2016-11-21 14:14:15 +0100

    [BURROW] get burrow out of the confluent stack

--- burrow/entrypoint.sh
+++ burrow/entrypoint.sh
@@ -4,6 +4,9 @@ BURROW_CONFIG_FILE=/burrow/config/burrow.cfg
 
 dockerize -template $BURROW_CONFIG_FILE.tmpl | grep -ve "^\s*$" | awk '{$1=$1};1' > $BURROW_CONFIG_FILE
 
+echo "Result config : "
+cat $BURROW_CONFIG_FILE
+
 if [ "$#" -eq 0 ]; then
 	# add zookeeper and kafka to the list of applications to wait for
 	WAIT_FOR_TCP="$BURROW_ZOOKEEPER_HOSTS,$BURROW_KAFKA_HOSTS,$WAIT_FOR_TCP"
@@ -14,7 +17,7 @@ if [ "$#" -eq 0 ]; then
 		done
 	fi
 	# launch burrow
-	if [[ -n "PAUSE_BEFORE_START" ]]; then
+	if [[ -n "$PAUSE_BEFORE_START" ]]; then
 		echo "Pausing $PAUSE_BEFORE_START seconds before start"
 		sleep $PAUSE_BEFORE_START
 	fi
